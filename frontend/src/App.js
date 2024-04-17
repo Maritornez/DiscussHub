eddceb2ea310391d70b2a0814d381dd65d82eeba
George Potonov <geopotonov@gmail.com>
2024-04-17 23:10:17 +0300

    Some changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Theme from './components/Theme';
+import ThemePage from './components/ThemePage';
 
 function App() {
+  const [themes, setThemes] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div className="left-panel">
+          <Routes>
+            <Route exact path="/" element={<Theme themes={themes} setThemes={setThemes} />} />
+            <Route path="/theme/:id" element={<ThemePage />} />
+          </Routes>
+        </div>
+        <div className="right-panel">
+          {/* Другие компоненты или контент */}
+        </div>
+      </div>
+    </Router>
   );
 }
 
