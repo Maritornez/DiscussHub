8c45ca4b11935903d6d3f91f95001a9b7425d4d1
George Potonov <geopotonov@gmail.com>
2024-11-23 08:51:12 +0300

    Обновление зависимостей во фронтенде

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,5 @@
-//const WebAPI_URL = "http://localhost:5000/api/";
-const WebAPI_URL = "https://localhost:7200/api/";
+const WebAPI_URL = "http://localhost:5000/api/";
+//const WebAPI_URL = "http://localhost:5278/api/";
+//const WebAPI_URL = "https://localhost:7200/api/";
 //const WebAPI_URL = "https://localhost:44343/api/";
 export default WebAPI_URL;
